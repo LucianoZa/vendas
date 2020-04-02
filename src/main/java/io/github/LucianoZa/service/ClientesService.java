c7449b00c13ae2ef6926388864ffd5e11618a18b
Luciano <lucianoza@gmail.com>
2020-04-02 20:14:47 -0300

    Application.Properties

--- src/main/java/io/github/LucianoZa/service/ClientesService.java
+++ src/main/java/io/github/LucianoZa/service/ClientesService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClientesService {
 
-    private ClientesRepository repository];
+    private ClientesRepository repository;
 
     @Autowired
     public ClientesService(ClientesRepository repository){
