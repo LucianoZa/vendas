aee5d1b3fa5588574d5898833f7a026d08eabd72
Luciano <lucianoza@gmail.com>
2020-04-02 21:40:09 -0300

    Anotations Customizadas

---
+++ src/main/java/io/github/LucianoZa/Animal.java
@@ -0,0 +1,5 @@
+package io.github.LucianoZa;
+
+public interface Animal {
+    void fazerbarulho();
+}
--- src/main/java/io/github/LucianoZa/VendasApplication.java
+++ src/main/java/io/github/LucianoZa/VendasApplication.java
@@ -3,20 +3,29 @@ package io.github.LucianoZa;
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.beans.factory.annotation.Qualifier;
     import org.springframework.beans.factory.annotation.Value;
+    import org.springframework.boot.CommandLineRunner;
     import org.springframework.boot.SpringApplication;
     import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.context.annotation.Bean;
     import org.springframework.web.bind.annotation.GetMapping;
     import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@ComponentScan() normalmente desnecessário explicitar! Necessario caso haja projeto externo.
 @RestController
 public class VendasApplication {
 
-    //@Autowired
-    //@Qualifier("applicationName")
     @Value("${application.name}")
     private String applicationName;
+
+    @Gato
+    private Animal animal;
+
+    @Bean
+    public CommandLineRunner executarAnimal() {
+        return args -> {
+            this.animal.fazerbarulho();
+        };
+    }
 
     @GetMapping("/hello")
     public String helloworld() {
