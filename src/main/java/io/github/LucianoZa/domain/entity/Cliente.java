e09ce2606887d36d394347882253eac28b6fb219
Luciano <lucianoza@gmail.com>
2020-04-05 11:42:28 -0300

    Entitys domain.entity

--- src/main/java/io/github/LucianoZa/domain/entity/Cliente.java
+++ src/main/java/io/github/LucianoZa/domain/entity/Cliente.java
@@ -1,4 +1,24 @@
 package io.github.LucianoZa.domain.entity;
 
+import java.math.BigDecimal;
+
 public class Cliente {
+    private Integer id;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
